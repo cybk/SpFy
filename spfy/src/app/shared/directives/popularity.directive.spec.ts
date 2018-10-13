db0dcf03178984e4508836ee1e4f5a6bda31b4da
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-10-12 21:02:50 -0500

    Updated the tests

--- spfy/src/app/shared/directives/popularity.directive.spec.ts
+++ spfy/src/app/shared/directives/popularity.directive.spec.ts
@@ -1,4 +1,10 @@
 import { PopularityDirective } from './popularity.directive';
+import { Component } from '@angular/core';
+
+@Component({
+  template: `<div> [appPopularity]='90'</div>`
+})
+class TestComponent{}
 
 describe('PopularityDirective', () => {
   it('should create an instance', () => {
--- spfy/src/app/welcome/welcome.component.spec.ts
+++ spfy/src/app/welcome/welcome.component.spec.ts
@@ -1,14 +1,34 @@
+import { PopularityDirective } from './../shared/directives/popularity.directive';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CardComponent } from './../shared/card/card.component';
+import { CardHolderComponent } from './../shared/card-holder/card-holder.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/Common/http/testing';
 
 import { WelcomeComponent } from './welcome.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      declarations: [
+        WelcomeComponent,
+        CardHolderComponent,
+        CardComponent,
+        PopularityDirective
+      ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+
     })
     .compileComponents();
   }));
@@ -16,10 +36,16 @@ describe('WelcomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
+    el =  fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should have a welcome message', () => {
+    const welcome = el.query( By.css('h1'));
+    expect(welcome.nativeElement.innerText).toEqual('Welcome');
+  })
 });
